44344eafd7fb5adc0b844f5e0bbda5eeef446a42
wrogoz <wrogoz@gmail.com>
2019-02-13 23:25:58 +0100

    project restarted - basic settings

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from "react";
-import './app.css'
+import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavbarToggler, MDBCollapse,MDBRow,MDBCol, MDBNavItem, MDBFooter, MDBNavLink, MDBContainer } from "mdbreact";
+
+import { BrowserRouter as Router } from "react-router-dom";
+import Routes from "./Routes";
 
 class App extends Component {
 
-render(){
-  return <div>
-    <h1 className='welcome'>test scss</h1>
-  </div>
-}
-}
+  render(){
+    return (
+    <MDBContainer fluid>
+        <MDBRow>
+          <MDBCol md="12">.col-md-12</MDBCol>
+        </MDBRow>
+        <MDBRow>
+          <MDBCol md="3">.col-md-3</MDBCol>
+          <MDBCol md="9">.col-md-9</MDBCol>
+        </MDBRow>
+
+    </MDBContainer>)
+  }
+  
+};
 
 export default App;
