54e549322a92495ec9b7c40d48978ac28b31b307
Wojciech Rogóż <wrogoz@gmail.com>
2019-02-25 08:46:33 +0100

    starter page css upgrade, navigation ul centered, footer padding corrections

--- src/components/MainBox/StartPage/start.js
+++ src/components/MainBox/StartPage/start.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { MDBCol, MDBRow} from "mdbreact";
-
+import './startPage.css'
 const Start = () => {
     
     return (
-        <MDBRow className="mainBox no-gutters">
-            <MDBCol md="12" >
+        <MDBRow className="mainBox no-gutters ">
+            <MDBCol md="12" className=" starter">
                <h1>Siemaneczko </h1>
-               <h2>Zapraszam Cię do przeżycia niesamowitej przygody jaką jest poznanie własnego ciała</h2>
-               <p>Po lewej znajdziesz menu w którym znajdziesz wiele ciekawych tematów z którymi warto się zapoznać</p>
-               <h3>Udanej zabawy:)</h3>
+               <h2>Nazywam się Tomek i zapraszam Cię do przeżycia niesamowitej przygody jaką jest poznanie własnego ciała.</h2>
+               <p>Po lewej znajdziesz menu w którym znajdziesz wiele ciekawych tematów z którymi warto się zapoznać.</p>
+               <h3>Udanej zabawy!</h3>
             </MDBCol>
         </MDBRow>
     )
